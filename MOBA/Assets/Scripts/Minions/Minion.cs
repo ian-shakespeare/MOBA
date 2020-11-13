8d5d00ac11d7a13e4d8d6bbc450e387c958e54f6
ArcticStorm9 <IRshakes11@gmail.com>
2020-11-13 14:15:10 -0700

    Minions

---
+++ MOBA/Assets/Scripts/Minions/Minion.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour {
+  public float MoveSpeed;
+  public bool isFriendly;
+
+  void Update() {
+    GameObject Enemy = enemyInRange();
+    if ( Enemy != null ) {
+      attack( Enemy );
+    }
+    else {
+      move();
+    }
+  }
+  void attack( GameObject enemy ) {
+    //Debug.Log("Attacking");
+  }
+  void move() {
+    //Debug.Log("Moving");
+    transform.Translate ( Vector3.forward * MoveSpeed * Time.deltaTime );
+  }
+  void OnTriggerEnter( Collider other ) {
+    if ( other.gameObject == GameObject.Find("Projectile") ) {
+      Debug.Log("Projectile Collision");
+    }
+  }
+  GameObject enemyInRange() {
+    Collider[] hitColliders = Physics.OverlapSphere( transform.position, 1.7f );
+    foreach ( var hitCollider in hitColliders ) {
+      if ( hitCollider.tag == "Entity" && hitCollider.gameObject != this.gameObject && getOtherFriendly( hitCollider.gameObject ) != getIsFriendly() ) {
+        return hitCollider.gameObject;
+      }
+    }
+    return null;
+  }
+  public void setIsFriendly( bool friendly ) {
+    isFriendly = friendly;
+  }
+  public bool getIsFriendly() {
+    return isFriendly;
+  }
+  public bool getOtherFriendly( GameObject entity ) {
+    /*if ( entity.GetType() == typeof(Player) ) {
+      Player player = entity.gameObject.GetComponent<Player>();
+      return player.getIsFriendly();
+    }
+    else*/ if ( entity.GetComponent(typeof(Minion)) != null ) {
+      Minion minion = entity.gameObject.GetComponent<Minion>();
+      return minion.getIsFriendly();
+    }
+    return false;
+  }
+}
---
+++ MOBA/Assets/Scripts/Minions/MinionSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionSpawner : MonoBehaviour {
+      public bool isFriendly;
+      public Minion minion;
+      private Quaternion SpawnRotation;
+      private int frames = 0;
+
+      // Start is called before the first frame update
+      void Start() {
+        switch( isFriendly ) {
+          case true:
+            SpawnRotation = Quaternion.Euler(0, 45, 0);
+            minion.setIsFriendly(true);
+            Instantiate( minion, new Vector3( -9, 0, -9), SpawnRotation );
+            break;
+          case false:
+            SpawnRotation = Quaternion.Euler(0, -135, 0);
+            minion.setIsFriendly(false);
+            Instantiate( minion, new Vector3( 9, 0, 9), SpawnRotation );
+            break;
+        }
+      }
+
+      // Update is called once per frame
+      void Update() {
+        if ( frames == 60 ) {
+          switch( isFriendly ) {
+            case true:
+              SpawnRotation = Quaternion.Euler(0, 45, 0);
+              minion.setIsFriendly(true);
+              Instantiate( minion, new Vector3( -9, 0, -9), SpawnRotation );
+              break;
+            case false:
+              SpawnRotation = Quaternion.Euler(0, -135, 0);
+              minion.setIsFriendly(false);
+              Instantiate( minion, new Vector3( 9, 0, 9), SpawnRotation );
+              break;
+          }
+        }
+        frames += 1;
+      }
+}
